75d76d5578c5862104f76979cc842abcff41b914
Brendan Benner <brendanbenner@berkeley.edu>
2021-01-24 12:59:10 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render (){
+    return (
+      <div className="App">
+        <div styles="margin-top:50px;">
+          <div id="title">Rise Network Report #1</div>
+          <div id="viewer" className="spreads"></div>
+          <div styles="text-align:center; margin:auto; font-size:100px;">
+            <button type="button" className="btn btn-primary btn-lg arrow" id="prev" href="#prev">‹</button>
+            <button type="button" className="btn btn-primary btn-lg arrow" id="next" href="#next">›</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
